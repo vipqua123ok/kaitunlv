95f08e05ecfe64fbc037038ebc63be220bae70e7
vipqua123ok <nnsads005@gmail.com>
2024-11-06 18:24:30 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,53 @@
+_G.Quest = { 
+    ['RGB Aura Haki'] = true,
+    ['Pull Lever'] = true,
+    ['Quest Dough Awaken'] = true
+}
+_G.Race = { 
+    ['Select Race'] = {'Human','Fishman','Skypiea','Mink'},
+    ['Lock Race'] = false,
+    ['Evo Race V3'] = false
+}
+_G.Melee = { 
+    ['Godhuman'] = false
+}
+_G.Sword = { 
+    ['Saber'] = false,
+    ['Midnight Blade'] = false,
+    ['Shisui'] = false,
+    ['Saddi'] = false,
+    ['Wando'] = false,
+    ['Yama'] = false,
+    ['Koko'] = false,
+    ['Rengoku'] = false,
+    ['Canvander'] = false,
+    ['Buddy Sword'] = false,
+    ['Twin Hooks'] = false,
+    ['SpikeyTrident'] = false,
+    ['Hallow Scryte'] = false,
+    ['Dark Dagger'] = false,
+    ['Tushita'] = false,
+    ['True Triple Katana'] = false,
+    ['Cursed Dual Katana'] = false,
+    ['Shark Anchor'] = false
+}
+_G.Gun = {  
+    ['Kabucha'] = false,
+    ['Acidum Rifle'] = false,
+    ['Soul Guitar'] = false, 
+    ['Serpent Bow'] = false
+}
+_G.Fruit = { 
+    ['Main Fruit'] = {'Flame-Flame'},
+    ['Select Fruit'] = {'Flame-Flame','Ice-Ice','Sand-Sand','Dark-Dark','Light-Light','Magma-Magma','Quake-Quake','Spider-Spider'}
+}
+_G.Mastery = { 
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['Fruit'] = true
+}
+_G.Setting = {
+    ['FPS Booster'] = true,
+    ['Auto Close Ui'] = true
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bestluxca/CrackedFree/main/Axionix%20Kaitun"))()
